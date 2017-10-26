6964b0212d5f37929c1aaeb719425e6425c15818
niwatorihada <i1788106@cse.kyoto-su.ac.jp>
2017-10-27 06:28:15 +0900

    登録者の情報を格納するMember classの雛形を作成.

---
+++ src/main/java/Member.java
@@ -0,0 +1,32 @@
+/**
+ * 登録者の情報を格納するクラス
+ * sugihara
+ * 2017/10/27
+ **/
+
+public class Member {
+    public String name;
+    public String ip;
+
+    public Member(String name, String ip) {
+        setName(name);
+        setIp(ip);
+    }
+    
+    // name のsetter
+    public void setName(String name) {
+        this.name = name;
+    }
+    // name のgetter
+    public String getName() {
+        return this.name;
+    }
+    // ip のsetter
+    public void setIp(string ip) {
+        this.ip = ip;
+    }
+    // ip のgetter
+    public String getIp() {
+        return this.ip;
+    }
+}
