22df5c43b96905fa55bc0ea74170d510343d65cd
K.Yoshigami <2017043@softvi.info>
2017-11-10 10:18:19 +0900

    横井君のブランチからWebSocket系統のファイルをいただいた

---
+++ src/main/webapp/js/websocket.js
@@ -0,0 +1,50 @@
+
+// websocketオブジェクト
+var ws;
+
+// ws接続押下時の処理
+window.onload = function() {
+
+	// WebSocketオブジェクト作成
+	ws = new WebSocket('ws://' + window.location.host + '/lama/ws');
+	//　本番環境用
+	//ws = new WebSocket('wss://' + window.location.host + '/lama/ws');
+	
+	
+	update();
+
+	// サーバからのメッセージ受信時の処理
+	ws.onmessage = function(message) {
+		//message = JSON.parse(message.data)
+		message = message.data.split(":")
+		console.log(message)
+		// TODO
+		// DOM操作してHTMLに反映
+		//$('#log').append('<p>' + message.data + '</p>');	
+		if(message[0] == "post-task") {
+			insertTask(message[1]);
+		}
+		else if(message[0] == "delete-task") {
+			deleteTask(message[1]);
+		}
+		else if(message[0] == "change-task-body")
+			updateTaskBody(message[1]);
+	};
+	
+	ws.onclose = function(closeEvent) {
+	    console.log('code = ' + closeEvent.code + ', reason = ' + closeEvent.reason);
+	};
+
+
+}
+
+// send押下時の処理
+$('#send').click(function() {
+    // TODO
+	// テキストデータをinputフィールドから読み込む
+	var body = $('#body').val();
+	
+	// ws経由で送信
+	ws.send(body);
+});
+
