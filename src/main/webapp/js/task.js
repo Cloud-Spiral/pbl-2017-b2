a8b4f48db9f49f87c1802f1ee9ebfe0602d8c59b
nessiris <g1345512@cse.kyoto-su.ac.jp>
2017-11-14 16:33:37 +0900

    #31 todoを内容を編集できるフォームを作った

--- src/main/webapp/js/task.js
+++ src/main/webapp/js/task.js
@@ -8,6 +8,9 @@ document.write("<script type='text/javascript' src='js/Moment.js'></script>");
 var postTask = function() {
 	var message = $('#message').val();
 	var priority = $('#priority').raty('score');
+	if(message == "") {
+		return;
+	}
 	$.ajax({
 		type : 'POST',
 		url : endpoint + '/tasks',
@@ -52,7 +55,9 @@ var insertTask = function(tid) {
 			td4 = tr.insertCell(-1);
 
 			var symbol = '<img src=image/default-symbol.png id=task-symbol' + task.tid + '>',
-			body = '<input id=task-body' + task.tid + ' value=' + task.body +' type="text" onkeyup="changeTaskBody(this)">',
+			//body = '<input id=task-body' + task.tid + ' value=' + task.body +' type="text" onkeyup="changeTaskBody(this)">',
+			body = '<input id=task-body' + task.tid + ' value=' + task.body +' type="text" onclick="showEditTaskBodyForm(this)" readonly="readonly">';
+			
 			//date = moment(task.date).format('YYYY年MM月DD日 HH時mm分'),
 			priority = '<class=task-priority id=task-priority' + task.tid + '>', //oninpupt
 			status = '<input id=task-status' + task.tid + ' type="button" value="' + buttonStatus + '" onclick="changeTaskStatus(this)">'
@@ -60,11 +65,13 @@ var insertTask = function(tid) {
 			td2.innerHTML = body;
 			td3.innerHTML = priority;
 			td4.innerHTML = status;
-
+			
 			$("#task-symbol"+task.tid).parent().addClass('symbol-cell');
 			$("#task-body"+task.tid).parent().addClass('body-cell');
 			$("#task-priority"+task.tid).parent().addClass('priority-cell');
 			$("#task-status"+task.tid).parent().addClass('status-cell');
+			
+			$("#task-symbol"+task.tid).parent().parent().attr('id','task-id'+task.tid);
 
 			$("#task-priority"+task.tid).raty({
 				number:5,
@@ -76,6 +83,20 @@ var insertTask = function(tid) {
 			if(task.status == "open") {
 				changeSymbol(task);
 			}
+			
+			//入力用
+			$("#task-id"+task.tid).after(
+					'<tr id=edit-task-body' + task.tid +' style=display:none>'
+					+ '<td class=symbol-cell><div id=cell' + task.tid + '></div></td>'
+					+ '<td class=body-cell>'
+					+ '<div id=cell' + task.tid + '>'
+					+ '<input id=task-body-edit' + task.tid + ' value=' + task.body +' type="text" onkeypress="changeTaskBody(this)">'
+					+ '</div>'
+					+ '</td>'
+					+ '<td class=priority-cell><div id=cell' + task.tid + '></div></td>' 
+					+ '<td class=status-cell><div id=cell' + task.tid + '></div></td>'
+					+ '</tr>');
+			
 		}
 	});
 }
@@ -84,6 +105,9 @@ var insertTask = function(tid) {
 var deleteTask = function(tid) {
 	var target = document.getElementById("task-status"+tid);
 	tr = target.parentNode.parentNode;
+	tr.parentNode.deleteRow(tr.sectionRowIndex);
+	
+	tr = document.getElementById("edit-task-body"+tid);
 	tr.parentNode.deleteRow(tr.sectionRowIndex);
 }
 
@@ -123,27 +147,52 @@ var changeSymbol = function(task) {
 	}
 }
 
+//入力フォームを出す
+var showEditTaskBodyForm = function(task) {
+	
+	var tid = task.id.replace("task-body","");
+
+	if($("#edit-task-body"+tid).css("display") != "none") {
+		document.activeElement.blur()
+	}
+	$('#cell'+tid).toggle('blind', '', 300);
+	$('#edit-task-body'+tid).toggle('blind', '', 300);
+	
+	var body = document.getElementById('task-body-edit'+tid);
+	body.value = $('#task-body-edit'+tid).val();
+	body.focus();
+	body.setSelectionRange(body.value.length, body.value.length);
+	
+}
+
+
 //taskの内容を変更
 var changeTaskBody = function(task) {
 	var key = window.event.keyCode;
-	//if(key == 13) {
-	var tid = task.id.replace("task-body","");
-	var body = $('#task-body'+tid).val();
-	var priority = $('#task-priority'+tid).raty('score');
-	if(body == "") return;
-	$.ajax({
-		type : 'PUT',
-		url : endpoint + '/tasks',
-		data: {
-			tid: tid,
-			body: body,
-			priority: priority
-		},
-		success : function(data) {
-			tws.send("change-task-body:"+tid)
+	if(key == 13) {
+		var tid = task.id.replace("task-body-edit","");
+		var body = $('#task-body-edit'+tid).val();
+		var priority = $('#task-priority'+tid).raty('score');
+		if(body == "") {
+			showEditTaskBodyForm(task)
+			return;
 		}
-	});
-	//}
+		$.ajax({
+			type : 'PUT',
+			url : endpoint + '/tasks',
+			data: {
+				tid: tid,
+				body: body,
+				priority: priority
+			},
+			success : function(data) {
+				tws.send("change-task-body:"+tid)
+				document.activeElement.blur()
+				$('#cell'+tid).toggle('blind', '', 300);	
+				$('#edit-task-body'+tid).toggle('blind', '', 300);
+			}
+		});
+	}
 }
 
 //taskの優先度を変更
@@ -175,12 +224,25 @@ var createTaskTable = function(tasks) {
 			buttonStatus = "open";
 		$('<tr id=task-id' + tasks[i].tid +'>'
 				+ '<td class=symbol-cell><img src="image/default-symbol.png" id=task-symbol' + tasks[i].tid + '></td>'
-				+ '<td class=body-cell><input id=task-body' + tasks[i].tid + ' value=' + tasks[i].body +' type="text" onkeyup="changeTaskBody(this)"></td>'
+				+ '<td class=body-cell><input id=task-body' + tasks[i].tid + ' value=' + tasks[i].body +' type="text" onclick="showEditTaskBodyForm(this)" readonly="readonly"></td>'
 				//+ '<td>' + moment(tasks[i].date).format('YYYY年MM月DD日 HH時mm分') + '</td>'
 				+ '<td class=priority-cell class=task-priority id=task-priority' + tasks[i].tid + '></td>' 
 				+ '<td class=status-cell><input id=task-status' + tasks[i].tid + ' type="button" value="' + buttonStatus + '" onclick="changeTaskStatus(this)"></td>'
 				+ '</tr>')
 				.appendTo('table#' + tasks[i].status + '-tasks tbody');
+		
+		$('<tr id=edit-task-body' + tasks[i].tid +' style=display:none>'
+				+ '<td class=symbol-cell><div id=cell' + tasks[i].tid + '></div></td>'
+				+ '<td class=body-cell>'
+				+ '<div id=cell' + tasks[i].tid + '>'
+				+ '<input id=task-body-edit' + tasks[i].tid + ' value=' + tasks[i].body +' type="text" onkeypress="changeTaskBody(this)">'
+				+ '</div>'
+				+ '</td>'
+				+ '<td class=priority-cell><div id=cell' + tasks[i].tid + '></div></td>' 
+				+ '<td class=status-cell><div id=cell' + tasks[i].tid + '></div></td>'
+				+ '</tr>')
+				.appendTo('table#' + tasks[i].status + '-tasks tbody');
+		
 		$("#task-priority"+tasks[i].tid).raty({
 			number:5,
 			score: tasks[i].priority,
